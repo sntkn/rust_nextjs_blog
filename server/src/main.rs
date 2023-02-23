51f2c56e70c95f6a20c355aa9605812a1d3de390
sntkn <mail.tsurumi@gmail.com>
2023-02-23 17:53:04 +0900

    graphql hello world

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,38 @@
-#[rocket::launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", rocket::routes![hello])
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema, SimpleObject};
+use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
+use rocket::{response::content, State};
+
+#[derive(SimpleObject)]
+struct QueryRoot {
+    /// Value a
+    say: String,
 }
 
+pub type BlogSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
 #[rocket::get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn graphiql() -> content::RawHtml<String> {
+    content::RawHtml(GraphiQLSource::build().endpoint("/graphql").finish())
+}
+
+#[rocket::get("/graphql?<query..>")]
+async fn graphql_query(schema: &State<BlogSchema>, query: GraphQLQuery) -> GraphQLResponse {
+    query.execute(schema.inner()).await
+}
+
+#[rocket::post("/graphql", data = "<request>", format = "application/json")]
+async fn graphql_request(schema: &State<BlogSchema>, request: GraphQLRequest) -> GraphQLResponse {
+    request.execute(schema.inner()).await
+}
+
+#[rocket::launch]
+fn rocket() -> _ {
+    let query_root = QueryRoot {
+        say: "Hello, world!".to_string(),
+    };
+    let schema = Schema::build(query_root, EmptyMutation, EmptySubscription).finish();
+    rocket::build().manage(schema).mount(
+        "/",
+        rocket::routes![graphql_query, graphql_request, graphiql],
+    )
 }
