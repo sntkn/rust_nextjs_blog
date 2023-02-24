6222e0486bcf159f938fdfeb05f1e8b40cf0202d
sntkn <mail.tsurumi@gmail.com>
2023-02-24 21:37:48 +0900

    sqlite query

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,23 @@
-use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema, SimpleObject};
+use anyhow::Result;
+use async_graphql::{
+    http::GraphiQLSource, Context, EmptyMutation, EmptySubscription, Object, Schema,
+};
 use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
 use rocket::{response::content, State};
+use sqlx::SqlitePool;
 
-#[derive(SimpleObject)]
-struct QueryRoot {
-    /// Value a
-    say: String,
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn hello<'ctx>(&self, ctx: &Context<'ctx>) -> Result<String> {
+        let pool = ctx.data::<SqlitePool>().unwrap();
+        let (res,): (String,) = sqlx::query_as("select 'World!'").fetch_one(pool).await?;
+        Ok(res)
+    }
 }
 
-pub type BlogSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+type BlogSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
 #[rocket::get("/")]
 fn graphiql() -> content::RawHtml<String> {
@@ -26,11 +35,13 @@ async fn graphql_request(schema: &State<BlogSchema>, request: GraphQLRequest) ->
 }
 
 #[rocket::launch]
-fn rocket() -> _ {
-    let query_root = QueryRoot {
-        say: "Hello, world!".to_string(),
-    };
-    let schema = Schema::build(query_root, EmptyMutation, EmptySubscription).finish();
+async fn rocket() -> _ {
+    let pool = SqlitePool::connect("sqlite://db.sqlite?mode=rwc")
+        .await
+        .unwrap();
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(pool)
+        .finish();
     rocket::build().manage(schema).mount(
         "/",
         rocket::routes![graphql_query, graphql_request, graphiql],
