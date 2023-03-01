b1b0d12dfb4ade8c1fc747ed0510048cf520dc06
sntkn <mail.tsurumi@gmail.com>
2023-03-01 23:21:38 +0900

    add CORS

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,7 +5,8 @@ use async_graphql::{http::GraphiQLSource, EmptySubscription, Schema};
 use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
 use db_pool::DbPool;
 use models::{mutation::MutationRoot, query::QueryRoot, BlogSchema};
-use rocket::{response::content, State};
+use rocket::{http::Method, response::content, State};
+use rocket_cors::AllowedOrigins;
 
 #[rocket::get("/")]
 fn graphiql() -> content::RawHtml<String> {
@@ -25,11 +26,31 @@ async fn graphql_request(schema: &State<BlogSchema>, request: GraphQLRequest) ->
 #[rocket::launch]
 async fn rocket() -> _ {
     let pool = DbPool::pool().await.unwrap();
+
+    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:3000"]);
+
+    // You can also deserialize this
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Post, Method::Get]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        //allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap();
+
     let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
         .data(pool)
         .finish();
-    rocket::build().manage(schema).mount(
-        "/",
-        rocket::routes![graphql_query, graphql_request, graphiql],
-    )
+    rocket::build()
+        .manage(schema)
+        .mount(
+            "/",
+            rocket::routes![graphql_query, graphql_request, graphiql],
+        )
+        .attach(cors)
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useQuery } from "urql";
 import { createClient, Provider } from "urql";
 
 const client = createClient({
-  url: "http://localhost:8000",
+  url: "http://localhost:8000/graphql",
 });
 
 // postsQueryDocument is now fully typed!
@@ -19,15 +19,29 @@ const postsQueryDocument = graphql(/* GraphQL */ `
 `);
 
 const Posts = () => {
-  const { data } = useQuery({ query: postsQueryDocument });
-  return <div>Posts</div>;
+  const [result, reexecuteQuery] = useQuery({ query: postsQueryDocument });
+  const { data, fetching, error } = result;
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+  if (data === undefined) return <p>undefined...</p>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Posts</h1>
+      <ul>
+        {data.posts.map((post) => (
+          <li key={post.id} className="underline">
+            {post.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default function Home() {
   return (
     <Provider value={client}>
       <Posts />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </Provider>
   );
 }
